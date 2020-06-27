593906934e7890e658b86190711f0602cef47af1
Greg Allebach <greg.allebach@gmail.com>
2020-06-27 14:41:31 -0400

    completed sql update for when items are purchased

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,8 @@ var products = [];
 
 connection.connect(function (err) {
     if (err) throw err;
-    showInv()
+    showInv();
+  
 })
 
 function showInv() {
@@ -23,6 +24,7 @@ function showInv() {
             console.log("| " + res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity + " |");
             products.push(res[i]);
         }
+        purchase();
     })
 }
 
@@ -32,14 +34,43 @@ function purchase() {
             {
                 name: "selectProduct",
                 type: "input",
-                message: "Enter product ID of the item you would like to purchase"
+                message: "Enter product ID of the item you would like to purchase: "
             },
             {
                 name: "quantity",
                 type: "input",
-                message: "Enter the quantity you would like to purchase"
+                message: "Enter the quantity you would like to purchase: "
             }
-        ]).then(function(answer) {
-            
+        ]).then(function (answer) {
+            for (var i = 0; i < products.length; i++) {
+                if (products[i].id == answer.selectProduct) {
+                    var selectedItem = products[i];
+                    var qtyCheck = products[i].stock_quantity - answer.quantity;
+                    if (qtyCheck >= 0) {
+                        connection.query("UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: qtyCheck
+                                },
+                                {
+                                    id: answer.selectProduct
+                                }
+                            ],
+                            function (error) {                                
+                                if (error) throw error;
+
+                                var totalPrice = parseInt(answer.quantity) * parseFloat(selectedItem.price);
+
+                                console.log(answer.quantity + " " + selectedItem.product_name + "(s) have been purchased for a total of " + "$" + (Math.round(totalPrice * 100) / 100).toFixed(2) + "!")
+                            }
+                        );
+                        showInv();
+                    }
+                    else {
+                        console.log("Insufficient inventory!");
+                        purchase()
+                    }
+                }
+            }
         })
 }
